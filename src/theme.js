bdf68c060e723f024f52c6dab303345cc820813b
MYKYTA RYSIEV <nickpolska21@gmail.com>
2022-01-25 15:13:13 +0100

    theme added

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  color: {
+    white: "#fff",
+    black: "#000",
+    lightSlateGrey: "#778899",
+    lightGrey: "#d3d3d3",
+    deepSkyBlue: "#00bfff",
+    crimson: "#ec365b"
+  },
+  breakpoint: {
+    mobileMax360: 360,
+    mobileMax414: 414,
+  },
+};
